fcbe23cc14f65d886d2be3a3e9771b6307b24c74
huntersfuntime <hunterorges@gmail.com>
2018-04-10 19:47:25 -0600

    Hide api key base in Gitignore

---
+++ app.js
@@ -0,0 +1,59 @@
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
+
+
+const argv = yargs
+    .options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
+});
+
+
+
+const request = require('request');
+
+
+request({
+    url: "",
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
+    } else {
+        console.log('Unable to fetch weather');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2d1047e1ea738b623f1826afa4c635a2
